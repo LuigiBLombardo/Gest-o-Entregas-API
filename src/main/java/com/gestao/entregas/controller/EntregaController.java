cb5959e1edc0016e42153355a4c4d5a902ee4430
LUIGI BARBIERI LOMBARDO <luigi_lombardo@estudante.sesisenai.org.br>
2026-09-23 21:39:00 -0300

    feat: Documentando as Controllers

--- src/main/java/com/gestao/entregas/controller/EntregaController.java
+++ src/main/java/com/gestao/entregas/controller/EntregaController.java
@@ -3,6 +3,9 @@ package com.gestao.entregas.controller;
 import com.gestao.entregas.dto.EntregaResponseDTO;
 import com.gestao.entregas.dto.MotoristaResponseDTO;
 import com.gestao.entregas.service.EntregaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +20,50 @@ public class EntregaController {
         this.service = service;
     }
 
+    /**
+     * Esse método é para buscar uma entrega no banco de dados atráves do id solicitado
+     *
+     * @param id id da entrega que será buscada no banco.
+     * @return List de entrega com o id solicitado.
+     */
+    @Operation(
+            summary = "Buscar por ID",
+            description = "Esse método vai ser excutado quando um ADMIN ou USER quiser buscar uma entrega pelo ID."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Entrega encontrada com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Algo deu errado ao buscar essa entrega"
+            )
+    })
     @GetMapping("/api/entregas/{id}")
     public ResponseEntity<EntregaResponseDTO> buscarporID(@PathVariable Long id){
         return ResponseEntity.ok(service.buscarPorID(id));
     }
 
+    /**
+     * Esse método é para excluir uma entrega de acordo com o id solicitado
+     *
+     * @param id id da entrega que irá ser excluida
+     */
+    @Operation(
+            summary = "Excluir uma entrega",
+            description = "Esse método vai ser excutado quando o ADMIN quiser excluir uma entrega especifica."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Entrega excluida com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "ALgo de errado na tentativa de excluir essa entrega"
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> remover(@PathVariable Long id){
         service.remover(id);
--- src/main/java/com/gestao/entregas/controller/MotoristaController.java
+++ src/main/java/com/gestao/entregas/controller/MotoristaController.java
@@ -4,6 +4,9 @@ import com.gestao.entregas.dto.MotoristaRequestDTO;
 import com.gestao.entregas.dto.MotoristaResponseDTO;
 import com.gestao.entregas.entity.MotoristaEntity;
 import com.gestao.entregas.service.MotoristaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -30,11 +33,50 @@ public class MotoristaController {
         this.service = service;
     }
 
+    /**
+     * Esse método é para buscar todos os motoristas do banco de dados
+     *
+     * @return List de motoristas.
+     */
+    @Operation(
+            summary = "Buscar todos os motoristas",
+            description = "Esse método vai ser excutado quando um ADMIN ou USER quiser buscar todos os motoristas."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Motorista listado com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Algo deu errado ao listar os motoristas"
+            )
+    })
     @GetMapping
     public ResponseEntity<List<MotoristaResponseDTO>> listar(){
         return ResponseEntity.ok(service.listar());
     }
 
+    /**
+     * Esse método é para cadastrar um motorista no banco de dados
+     *
+     * @param requestDTO resquest do motorista com os dados
+     * @return response do motorista com os dados convertidos
+     */
+    @Operation(
+            summary = "Cadastrar um Motorista",
+            description = "Esse método vai ser excutado quando um ADMIN quiser cadastrar um motorista."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Motorista encontrado com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Algo deu errado ao cadastrar esse motorista"
+            )
+    })
     @PostMapping
     public ResponseEntity<MotoristaResponseDTO> cadastrar(@RequestBody @Valid MotoristaRequestDTO requestDTO){
         MotoristaResponseDTO responseDTO = service.cadastrar(requestDTO);
